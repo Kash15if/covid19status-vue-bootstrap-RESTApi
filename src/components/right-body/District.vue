4851a64f4df358e53cbe4473e476fbb220855740
Kash15if <kash15if@yahoo.com>
2021-04-10 15:02:50 +0530

    changed css

--- src/components/right-body/District.vue
+++ src/components/right-body/District.vue
@@ -1,180 +1,195 @@
 <template>
-  <div>
-    <!-- graph of a particular state , all 3-->
-    <h1>{{ state }} {{ "'s" + " " + "Data" }}</h1>
 
-    <!--graph container-->
-    <div class="row" style="margin-top: 5vh;">
-      <div
-        class="col-md-4 col-sm-12 text-center text-primary"
-        v-if="totalConfirmed.length != 0"
-      >
-        <h4><b>Total Case</b></h4>
-        <h6>{{}}</h6>
+<div>
 
-        <line-chart
-          :chartData="totalConfirmed"
-          label="# total cases till today"
-          bgcolor="#0066ff"
-        ></line-chart>
-      </div>
 
-      <div
-        class="col-md-4 col-sm-12 text-center text-success"
-        v-if="totalConfirmed.length != 0"
-      >
-        <h4><b>Recovered</b></h4>
-        <h6>{{}}</h6>
+                <!-- graph of a particular state , all 3-->
+                <h1>{{state}} {{"'s" + " " + "Data"}}</h1>
 
-        <line-chart
-          :chartData="totalRecovered"
-          label="# of people recovered till today"
-          bgcolor="#00b359"
-        ></line-chart>
-      </div>
+                <!--graph container-->
+                <div class="row" style="margin-top: 5vh;">
 
-      <div
-        class="col-md-4 col-sm-12 text-center text-danger"
-        v-if="totalConfirmed.length != 0"
-      >
-        <h4><b>Death</b></h4>
-        <h6>{{}}</h6>
+                    <div class="col-md-4 col-sm-12 text-center text-primary"  v-if="totalConfirmed.length != 0">
+                        <h4><b>New Cases</b></h4>
+                        <h6>{{  }}</h6>
 
-        <line-chart
-          :chartData="totalDeceased"
-          label="# of deaths till today"
-          bgcolor="#ff0000"
-        ></line-chart>
-      </div>
-    </div>
+                       <line-chart
+                        :chartData="totalConfirmed"
+                        label="# New cases today"
+                        bgcolor="#0066ff"
+                        ></line-chart>
+                    </div>
 
-    <!--graph container ends-->
+                    <div class="col-md-4 col-sm-12 text-center text-success"  v-if="totalConfirmed.length != 0">
+                        <h4><b>Recovered Today</b></h4>
+                        <h6>{{  }}</h6>
+
+                        <line-chart
+                        :chartData="totalRecovered"
+                        label="# of people recovered today"
+                        bgcolor="#00b359"
+                        ></line-chart>
+                    </div>
+
+                    <div class="col-md-4 col-sm-12 text-center text-danger"  v-if="totalConfirmed.length != 0">
+                        <h4><b>Today's Death</b></h4>
+                        <h6>{{  }}</h6>
+
+                        <line-chart
+                        :chartData="totalDeceased"
+                        label="# of todays deaths"
+                        bgcolor="#ff0000"
+                        ></line-chart>
+                    </div>
+
+                </div>
+
+                <!--graph container ends-->
+                
+
 
     <!--Table of all districts of a particular state-->
-    <h1 align="center">Districts of {{ state }}</h1>
-    <br /><br />
+    <h1 align="center">Districts of {{state}}</h1><br><br>
 
-    <div class="table-responsive-xl ">
-      <table class="table table-dark table-striped">
-        <thead>
-          <tr>
-            <th>District</th>
-            <th>Total cases</th>
-            <th>Recovered</th>
-            <th>Active</th>
-            <th>Deaths</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="x in districtName" :key="x">
-            <td class="text-primary">
-              <b>{{ x }}</b>
-            </td>
-            <td class="text-primary">
-              <b>{{ distTable[x].confirmed }}</b>
-            </td>
-            <td class="text-success">
-              <b>{{ distTable[x].recovered }}</b>
-            </td>
-            <td class="text-warning">
-              <b>{{ distTable[x].active }}</b>
-            </td>
-            <td class="text-danger">
-              <b>{{ distTable[x].deceased }}</b>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-  </div>
+
+
+   
+                    <div class="table-responsive-xl " >
+
+                        <table class="table table-dark table-striped">
+                            <thead>
+                                <tr>
+                                <th>District</th>
+                                <th>Total cases</th>
+                                <th>Recovered</th>
+                                <th>Active</th>
+                                <th>Deaths</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+      
+                                <tr v-for="x in districtName" :key="x">
+                                    
+                                    <td class="text-primary"><b>{{ x }}</b></td>
+                                    <td class="text-primary"><b>{{distTable[x].confirmed }}</b></td>
+                                    <td class="text-success"><b>{{ distTable[x].recovered }}</b></td>
+                                    <td class="text-warning"><b>{{distTable[x].active }}</b></td>
+                                    <td class="text-danger"><b>{{ distTable[x].deceased }}</b></td>
+                                    
+                                </tr>
+
+                            
+                                
+
+                            </tbody>
+                        </table>
+
+                    </div>
+
+</div>
+
 </template>
 
 <script>
-import { mapActions, mapGetters, mapState } from "vuex";
+import  { mapActions , mapGetters , mapState } from 'vuex';
 import LineChart from "../graph/graph.vue";
 
 export default {
-  name: "District",
-  components: {
-    LineChart,
+  name: 'District',
+  components:{
+      LineChart,
   },
-  data: function() {
-    return {
-      state: "",
-      code: "",
-      totalConfirmed: [],
-      totalRecovered: [],
-      totalDeceased: [],
-      districtName: [],
-      distTable: [],
-    };
+  data: function(){
+      return{
+          state: "",
+          code: "",
+          totalConfirmed: [],
+          totalRecovered : [],
+          totalDeceased : [],
+          districtName: [],
+          distTable: [],
+
+      };
   },
 
-  methods: {
-    ...mapActions(["getData"]),
+  methods:{
+        ...mapActions(['getData']),
   },
 
   computed: {
-    ...mapGetters(["districts", "statesgraph"]),
-    ...mapState(["districts", "statesgraph"]),
+      ...mapGetters(['districts','statesgraph']),
+      ...mapState(['districts' , 'statesgraph'])
   },
 
-  created() {
-    this.getData()
-      .then(() => {
-        this.state = this.$route.query.state;
-        this.code = this.$route.query.code;
-      })
-      .then(() => {
-        // logic for graph to separate data
-        const statesGraph = this.statesgraph.states_daily;
-        let i = 1;
+  created(){
+      this.getData().then(() => {
+      this.state = this.$route.query.state;
+      this.code = this.$route.query.code;
+      }).then(() => {
+          
+          // logic for graph to separate data
+          const statesGraph =  this.statesgraph.states_daily;
+            let i = 1;
 
-        const stCode = this.code.toLowerCase();
+            const stCode = this.code.toLowerCase();
 
-        statesGraph.forEach((d) => {
-          const date = d.date;
+            var skipDate = 1;
+            statesGraph.forEach((d) => {
 
-          if (i == 1) {
+            if(skipDate > 270){
+  
+            const date = d.date;
+            
+            if(i == 1){
             const conf = d[stCode];
             this.totalConfirmed.push({ date, data: conf });
-          }
-          if (i == 2) {
-            const recov = d[stCode];
-            this.totalRecovered.push({ date, data: recov });
-          }
-          if (i == 3) {
-            const death = d[stCode];
-            this.totalDeceased.push({ date, data: death });
-            i = 0;
-          }
+            }
+            if( i == 2){
+                const recov = d[stCode];
+                this.totalRecovered.push({ date, data: recov });
+            
+            }
+            if( i == 3 ){
+                const death = d[stCode];
+                this.totalDeceased.push({ date, data: death });
+                i = 0;
+            }
 
-          i++;
-        });
-      })
-      .then(() => {
-        //logic for table
-        this.distTable = this.districts[this.state].districtData;
+            i++;
+            }
+            else{
+                
+                skipDate++;
+            }
+            
+            });
+      }).then(() => {
 
-        this.districtName = Object.keys(this.distTable);
+          //logic for table
+          this.distTable =  this.districts[this.state].districtData;
+          
+          this.districtName = Object.keys(this.distTable);
 
-        console.log(this.districtName);
+          console.log(this.districtName);
+          
       });
+
+      
   },
-};
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1,
-h3 {
-  text-align: center;
-  color: #0e6660;
-  margin: 5vh 5% 0 5%;
+h1 , h3 {
+    text-align: center;
+    color: #0e6660;
+    margin: 5vh 5% 0 5%;
+    
 }
 
-.table-responsive-xl {
-  padding: 0 2vw 0 2vw;
-  margin-top: 10vh;
+.table-responsive-xl{
+    padding: 0 2vw 0 2vw;
+    margin-top: 10vh;
 }
 </style>
--- src/components/right-body/Nationwide.vue
+++ src/components/right-body/Nationwide.vue
@@ -1,139 +1,155 @@
 <template>
-  <div>
-    <h1>Nationwide Data</h1>
+<div>
 
-    <!--graph container-->
-    <div class="row" style="margin-top: 5vh;">
-      <div
-        class="col-md-4 col-sm-12 text-center text-primary"
-        v-if="totalConfirmed.length != 0"
-      >
-        <h4><b>Total Case</b></h4>
-        <h6>{{ todayConfirmed }}</h6>
+                <h1>Nationwide Data</h1>
 
-        <line-chart
-          :chartData="totalConfirmed"
-          label="# total cases till today"
-          bgcolor="#0066ff"
-        ></line-chart>
-      </div>
+                <!--graph container-->
+                <div class="row" style="margin-top: 5vh;">
 
-      <div
-        class="col-md-4 col-sm-12 text-center text-success"
-        v-if="totalConfirmed.length != 0"
-      >
-        <h4><b>Recovered</b></h4>
-        <h6>{{ todayRecovered }}</h6>
+                    <div class="col-md-4 col-sm-12 text-center text-primary"  v-if="totalConfirmed.length != 0">
+                        <h4><b>New Cases</b></h4>
+                        <h6>{{ todayConfirmed }}</h6>
 
-        <line-chart
-          :chartData="totalRecovered"
-          label="# of people recovered till today"
-          bgcolor="#00b359"
-        ></line-chart>
-      </div>
+                       <line-chart
+                        :chartData="totalConfirmed"
+                        label="# New cases each today"
+                        bgcolor="#0066ff"
+                        ></line-chart>
+                    </div>
 
-      <div
-        class="col-md-4 col-sm-12 text-center text-danger"
-        v-if="totalConfirmed.length != 0"
-      >
-        <h4><b>Death</b></h4>
-        <h6>{{ todayDeceased }}</h6>
+                    <div class="col-md-4 col-sm-12 text-center text-success"  v-if="totalConfirmed.length != 0">
+                        <h4><b>Recovered daily</b></h4>
+                        <h6>{{ todayRecovered }}</h6>
 
-        <line-chart
-          :chartData="totalDeceased"
-          label="# of deaths till today"
-          bgcolor="#ff0000"
-        ></line-chart>
-      </div>
-    </div>
+                        <line-chart
+                        :chartData="totalRecovered"
+                        label="# of people recovered each day"
+                        bgcolor="#00b359"
+                        ></line-chart>
+                    </div>
 
-    <h3>Search states on Map</h3>
-    <div class="map">
-      <Map style="width: 100%; height: 80vh">
-        <MglMap :mapboxGl="mapbox - gl" @load="onMapLoaded"
-      /></Map>
-    </div>
-  </div>
+                    <div class="col-md-4 col-sm-12 text-center text-danger"  v-if="totalConfirmed.length != 0">
+                        <h4><b>Death</b></h4>
+                        <h6>{{ todayDeceased }}</h6>
+
+                        <line-chart
+                        :chartData="totalDeceased"
+                        label="# of deaths  each day"
+                        bgcolor="#ff0000"
+                        ></line-chart>
+                    </div>
+
+                </div>
+      
+                <h3>Search states on Map</h3>
+                <div class="map">
+                <Map style='width: 100%; height: 80vh'>
+                <MglMap
+                :mapboxGl="mapbox-gl"
+                @load="onMapLoaded"
+                /></Map>
+                </div>
+
+</div>
 </template>
 
 <script>
 import LineChart from "../graph/graph.vue";
-import { mapGetters, mapActions, mapState } from "vuex";
+import {mapGetters , mapActions , mapState } from 'vuex';
 import Map from "../graph/map.vue";
 
 export default {
-  name: "Nationwide",
+  name: 'Nationwide',
 
-  components: {
-    LineChart,
-    Map,
+  components:{
+      LineChart,
+      Map,
+  },
+  
+
+  data(){
+      return{
+          totalConfirmed: [],
+          totalRecovered : [],
+          totalDeceased : [],
+          todayConfirmed: "",
+          todayRecovered: "",
+          todayDeceased:  "",
+          
+      };
   },
 
-  data() {
-    return {
-      totalConfirmed: [],
-      totalRecovered: [],
-      totalDeceased: [],
-      todayConfirmed: "",
-      todayRecovered: "",
-      todayDeceased: "",
-    };
-  },
+    methods:{
+      ...mapActions(['getData'])
+    },
 
-  methods: {
-    ...mapActions(["getData"]),
-  },
+    computed: {
+      ...mapGetters(['nationwide']),
+      ...mapState(['nationwide'])
+    },
 
-  computed: {
-    ...mapGetters(["nationwide"]),
-    ...mapState(["nationwide"]),
-  },
+    created(){
+        this.getData().then(() => {
 
-  created() {
-    this.getData()
-      .then(() => {
-        const nationGraph = this.nationwide.cases_time_series;
-        let i = 0;
+            const nationGraph =  this.nationwide.cases_time_series;
+            let i = 0;
 
-        nationGraph.forEach((d) => {
-          const { date, totalconfirmed, totaldeceased, totalrecovered } = d;
-          if (i >= 60) {
-            this.totalConfirmed.push({ date, data: totalconfirmed });
-            this.totalRecovered.push({ date, data: totalrecovered });
-            this.totalDeceased.push({ date, data: totaldeceased });
-          }
+            nationGraph.forEach((d) => {
 
-          i++;
-        });
-      })
-      .then(() => {
-        this.todayConfirmed = this.nationwide.statewise[0].confirmed;
-        this.todayRecovered = this.nationwide.statewise[0].recovered;
-        this.todayDeceased = this.nationwide.statewise[0].deaths;
-      });
-  },
-};
+            const {
+                date,
+                dailyconfirmed,
+                dailydeceased,
+                dailyrecovered,
+                } = d;
+            if(i >= 150){
+            this.totalConfirmed.push({ date, data: dailyconfirmed });
+            this.totalRecovered.push({ date, data: dailyrecovered });
+            this.totalDeceased.push({ date, data: dailydeceased });
+            }
+
+            i++;
+            
+            });
+
+        }).then(() => {
+                
+            this.dailyConfirmed = this.nationwide.statewise[0].confirmed;
+            this.dailyRecovered = this.nationwide.statewise[0].recovered;
+            this.dailyDeceased = this.nationwide.statewise[0].deaths;
+
+            });
+        
+
+        
+        
+       
+    },
+    
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h4 {
-  margin-top: 3vh;
+
+h4{
+    margin-top: 3vh;
 }
 
-.col-sm-12 {
-  padding-top: 10vh;
+.col-sm-12{
+    padding-top: 10vh;
 }
 
-.map {
-  padding: 10vh 0vw 10vh 2vw;
-  height: 60%;
+.map{
+    padding: 10vh 0vw 10vh 2vw;
+    height: 60%;
 }
 
-h1,
-h3 {
-  text-align: center;
-  color: #0e6660;
-  margin-top: 5vh;
+h1 , h3 {
+    text-align: center;
+    color: #0e6660;
+    margin-top: 5vh;
 }
+
+
 </style>
